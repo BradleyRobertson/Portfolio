e61c4b7020d97f613e268267e6f66fff4d1afe71
Your Name <you@example.com>
2018-01-09 17:22:12 -0500

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
-// JavaScript Document
-var HomeSection = document.getElementById("HomeNav"),
-    PreviousSection = document.getElementById("PreviousNav"),
-    AboutSection = document.getElementById("AboutNav"),
-    ContactSection = document.getElementById("ContactNav");
 
-HomeSection.onclick = function() {
-  TweenLite.to(window, 1, {scrollTo:{y:"#HomeNav2", offsetY:70}});
+
+//lightboxes end //
+// scrollto thingo//
+
+
+  var HomeSection = document.querySelector("#HomeNav"),
+      PreviousSection = document.querySelector("#PreviousNav"),
+      AboutSection = document.querySelector("#AboutNav"),
+      ContactSection = document.querySelector("#ContactNav");
+
+      //console.log(HomeSection);
+
+      HomeSection.addEventListener("click", HomeSectionNav);
+          PreviousSection.addEventListener("click", PreviousSectionNav);
+              AboutSection.addEventListener("click", AboutSectionNav);
+                  ContactSection.addEventListener("click", ContactSectionNav);
+                //  console.log("this is loading fine");
+
+function HomeSectionNav() {
+  //console.log("from Home Section Nav");
+  TweenMax.to(window, 2, {ScrollTo:"#HomeNav2"});
+  //console.log("working1");
 }
 
-PreviousSection.onclick = function() {
-  TweenLite.to(window, 1, {scrollTo:{y:"#PreviousNav2", offsetY:70}});
+function PreviousSectionNav() {
+  TweenMax.to(window, 2, {scrollTo:"#Previousworks"});
+    //console.log("working2");
 }
 
-AboutSection.onclick = function() {
-  TweenLite.to(window, 1, {scrollTo:{y:"#AboutNav2", offsetY:70}});
+function AboutSectionNav() {
+  TweenMax.to(window, 2, {scrollTo:"#AboutNav2"});
+    //console.log("working3");
 }
 
-ContactSection.onclick = function() {
-  TweenLite.to(window, 1, {scrollTo:{y:"#ContactNav2", offsetY:70}});
+function ContactSectionNav() {
+  TweenMax.to(window, 2, {scrollTo:"#ContactNav2"});
+    //console.log("working4");
 }
